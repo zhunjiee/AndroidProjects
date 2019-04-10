014fc5c69c853af8690d6945ccf10edfccc1e726
zhunjiee <zhunjiee@qq.com>
2019-04-10 23:44:20 +0800

    AlertDialog,progressDialog 等控件

--- 04.UI/UIWidgetTest/app/src/main/java/com/zhunjiee/uiwidgettest/MainActivity.java
+++ 04.UI/UIWidgetTest/app/src/main/java/com/zhunjiee/uiwidgettest/MainActivity.java
@@ -1,11 +1,16 @@
 package com.zhunjiee.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.FileReader;
@@ -13,6 +18,10 @@ import java.io.FileReader;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         editText = findViewById(R.id.edit_text);
         imageView = findViewById(R.id.image_view);
+        progressBar = findViewById(R.id.progress_bar);
 
         Button button = findViewById(R.id.button);
 //        button.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +51,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 // 切换图片
                 imageView.setImageResource(R.drawable.android_2);
+
+                // 控制 progressBar 的显示与隐藏
+//                if (progressBar.getVisibility() == View.GONE) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                } else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);
+
+                // AlertDialog
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is a Dialog");
+                dialog.setMessage("Something important.");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "You press OK");
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "You press Cancel");
+                    }
+                });
+                dialog.show();
+
+                // ProgressDialog
+                // 8.0已废弃
+//                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+//                progressDialog.setTitle("This is ProgressDialog");
+//                progressDialog.setMessage("Loading...");
+//                progressDialog.setCancelable(true);
+//                progressDialog.show();
                 break;
+
                 default:
                     break;
         }
