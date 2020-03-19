501de202c0b164e848e24ecd6345cfe6ab9d6e4f
houbaowei <houbaowei@126.com>
2020-03-19 08:24:22 +0800

    百度地图

--- 12.BaiDuMap/LBSTest/app/src/main/java/com/example/lbstest/MainActivity.java
+++ 12.BaiDuMap/LBSTest/app/src/main/java/com/example/lbstest/MainActivity.java
@@ -30,11 +30,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
         setContentView(R.layout.activity_main);
 
         positionText = findViewById(R.id.position_text_view);
+
+        checkPermissionsAndRequestLocation();
+    }
+
+
+    /* 开始定位 */
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("纬度: ").append(bdLocation.getLatitude()).append("\n");
+            currentPosition.append("经度: ").append(bdLocation.getLongitude()).append("\n");
+            currentPosition.append("定位方式: ");
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
+                currentPosition.append("GPS");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                currentPosition.append("网络");
+            }
+            positionText.setText(currentPosition);
+        }
+    }
+
+
+    /*
+     * 检查权限
+     * */
+    private void checkPermissionsAndRequestLocation() {
         // 权限检查
         List<String> permissionList = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -54,48 +87,6 @@ public class MainActivity extends AppCompatActivity {
             requestLocation();
         }
     }
-
-    /* 开始定位 */
-    private void requestLocation() {
-        mLocationClient.start();
-    }
-
-    /**
-     * 申请权限
-     */
-    private void requestWriteSettings() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //大于等于23 请求权限
-            if ( !Settings.System.canWrite(getApplicationContext())) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
-            }
-        } else {
-            //小于23直接设置
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_WRITE_SETTINGS)
-        {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            {
-                //Settings.System.canWrite方法检测授权结果
-                if (Settings.System.canWrite(getApplicationContext()))
-                {
-                    Toast.makeText(this, "获取了权限", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(this, "您拒绝了权限", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-
-    }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -120,22 +111,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public class MyLocationListener extends BDAbstractLocationListener {
-
-        @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            StringBuilder currentPosition = new StringBuilder();
-            currentPosition.append("纬度: ").append(bdLocation.getLatitude()).append("\n");
-            currentPosition.append("经度: ").append(bdLocation.getLongitude()).append("\n");
-            currentPosition.append("定位方式: ");
-            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
-                currentPosition.append("GPS");
-            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
-                currentPosition.append("网络");
+    /**
+     * 单独申请 WRITE_SETTINGS 权限
+     */
+    private void requestWriteSettings() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //大于等于23 请求权限
+            if ( !Settings.System.canWrite(getApplicationContext())) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
             }
-            positionText.setText(currentPosition);
+        } else {
+            //小于23直接设置
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_WRITE_SETTINGS) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //Settings.System.canWrite方法检测授权结果
+                if (Settings.System.canWrite(getApplicationContext())) {
+                    Toast.makeText(this, "获取了权限", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, "您拒绝了权限", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 }
 
