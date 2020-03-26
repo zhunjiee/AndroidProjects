7efa7a816f025c0bed7c4b443c60c9aed41395b5
zhunjiee <zhunjiee@qq.com>
2020-03-26 21:36:52 +0800

    progressbar

--- Secondary/UIWidgetTest/app/src/main/java/com/zhunjiee/uiwidgettest/MainActivity.java
+++ Secondary/UIWidgetTest/app/src/main/java/com/zhunjiee/uiwidgettest/MainActivity.java
@@ -4,21 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edit_text);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                Toast.makeText(MainActivity.this, "You clicked button", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
